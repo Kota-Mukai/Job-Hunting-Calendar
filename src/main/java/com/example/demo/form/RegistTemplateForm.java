015507213a32c437119cd1321b51d0ad54465996
Kota_Mukai <kota1022@mukai@gmail.com>
2025-10-17 20:36:44 +0900

    pull request #6-2

--- src/main/java/com/example/demo/form/RegistTemplateForm.java
+++ src/main/java/com/example/demo/form/RegistTemplateForm.java
@@ -1,9 +1,13 @@
 package com.example.demo.form;
 
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class RegistTemplateForm {
 
     @Size(min = 1, max = 50, message = "登録するテンプレートの名前を1文字以上50文字以下で入力してください")
