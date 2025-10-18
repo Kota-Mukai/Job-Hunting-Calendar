b25651413d2a7f502be88f12a9e907428675c787
Kota_Mukai <kota1022@mukai@gmail.com>
2025-10-18 17:14:19 +0900

    pull request #8-2

--- src/main/java/com/example/demo/controller/RegistTemplateController.java
+++ src/main/java/com/example/demo/controller/RegistTemplateController.java
@@ -1,9 +1,14 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.form.CompanyForm;
 import com.example.demo.form.RegistTemplateForm;
 
 @Controller
@@ -11,7 +16,16 @@ import com.example.demo.form.RegistTemplateForm;
 public class RegistTemplateController {
 
     @GetMapping("regist-new-template")
-    public String showRegistForm(RegistTemplateForm form){
+    public String showRegistForm(
+        @ModelAttribute RegistTemplateForm form){
+
+        List<CompanyForm>  companyList = new ArrayList<>();
+
+        for(int i = 0; i < 5; i++){
+            companyList.add(new CompanyForm());
+        }
+
+        form.setCompanyList(companyList);
 
         return "regist-new-template";
     }
